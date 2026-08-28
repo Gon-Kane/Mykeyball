749689b0562398e26b1dbd2c2074e8b54f9f40c7
Gon-Kane <hiatal-tarter6e@icloud.com>
2026-08-28 13:14:53 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball61/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/rules.mk
@@ -14,11 +14,11 @@ LTO_ENABLE = yes
 
 # Build Options
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 
 # Duplex matrix.
