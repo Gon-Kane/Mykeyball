d0498ae3c750357177226728c5f649e02cddead6
Gon-Kane <hiatal-tarter6e@icloud.com>
2026-09-05 17:06:24 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball61/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/rules.mk
@@ -16,9 +16,10 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 
-# Duplex matrix.
-CUSTOM_MATRIX = lite
-SRC += lib/duplexmatrix/duplexmatrix.c
+//下の3行をコメント化 RP2040換装PJ
+//# Duplex matrix.
+//CUSTOM_MATRIX = lite
+//SRC += lib/duplexmatrix/duplexmatrix.c
 
 # Keyball61 is split keyboard.
 SPLIT_KEYBOARD = yes
