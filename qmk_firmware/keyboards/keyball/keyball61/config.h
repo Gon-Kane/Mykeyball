e1d540f0cc76e1a27ca784aef67213d13a9c5819
Gon-Kane <hiatal-tarter6e@icloud.com>
2026-09-24 23:23:25 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -24,10 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS     { D4, C6, D7, E6, B4 }
 #define MATRIX_COL_PINS     { F4, F5, F6, F7 }
 #define MATRIX_MASKED
-#define DEBOUNCE            20
+#define DEBOUNCE            15
 
 // マトリクススキャン時の安定待ち時間を長めにする（デフォルトは1〜5μs程度）1行
-#define MATRIX_IO_DELAY 140
+#define MATRIX_IO_DELAY 100
 
 // 起動時のUSB接続待ち時間を設定(2行)
 #define SPLIT_USB_TIMEOUT 2000
